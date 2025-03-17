b0b4ef3f96e04cd977bbfcd634e99e4501873f62
Mikita Stankevich <stankevich.mikita21@gmail.com>
2025-03-17 15:46:10 +0100

    Setting up the config yaml-file and config.go function

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,7 +1,15 @@
 package main
 
+import (
+	"fmt"
+	"go-url-shortener/internal/config"
+)
+
 func main() {
 	//TODO: init config: cleanenv
+	cfg := config.MustLoad()
+	fmt.Println(cfg)
+
 	//TODO: init logger: slog
 	//TODO: init storage: sqlite
 	//TODO: init router: chi
